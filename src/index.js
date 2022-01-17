f3c23365f9e6e95404a71541ccc70bb1d93dccf8
Károly Török <torok.karoly.krisztian@gmail.com>
2022-01-17 19:24:03 +0100

    Desktop-first media breakpoints

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 
 import App from './components/App';
 import GlobalStyles from './components/GlobalStyles';
 
+const BREAKPOINTS = {
+  phoneMax: 600,
+  tabletMax: 950,
+  laptopMax: 1300,
+};
+
+const theme = {
+  queries: {
+    phoneAndBelow: `(max-width: ${BREAKPOINTS.phoneMax / 16}rem)`,
+    tabletAndBelow: `(max-width: ${BREAKPOINTS.tabletMax / 16}rem)`,
+    laptopAndBelow: `(max-width: ${BREAKPOINTS.laptopMax / 16}rem)`,
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
     <GlobalStyles />
   </React.StrictMode>,
   document.getElementById('root')
